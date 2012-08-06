04c0ff5c9d8535b00d0b9a8bbea7f88f609489ad
Sam Morrison <sorrison@gmail.com>
2012-08-06 15:17:41 +1000

    Add ssl to rabbitmq

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,8 +5,21 @@ class rabbitmq {
   }
 
   service { 'rabbitmq-server':
-    ensure => running,
-    enable => true,
+    ensure  => running,
+    enable  => true,
+    require => Package['rabbitmq-server'],
+  }
+
+  file {'/etc/rabbitmq/ssl':
+    ensure  => directory,
+    require => Package['rabbitmq-server'],
+  }
+  
+  file {'/etc/rabbitmq/rabbitmq.config':
+    ensure  => present,
+    content => template("rabbitmq/rabbitmq.config.erb"),
+    notify  => Service['rabbitmq-server'],
+    require => Package['rabbitmq-server'],
   }
 
 }
