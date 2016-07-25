74b1d33ac557c9b3d2b6949fad7bfe7a3146822c
Andy Botting <andy@andybotting.com>
2016-07-25 15:50:17 +1000

    Add systemd limits.conf file required for v3.6.3
    Change-Id: Idacaf701b1df14d692bc5dc00294f54240edb0d7

--- manifests/init.pp
+++ manifests/init.pp
@@ -21,10 +21,30 @@ class rabbitmq(
     require => Package['rabbitmq-server'],
   }
 
-  file { '/etc/default/rabbitmq-server':
-    ensure  => present,
-    content => template('rabbitmq/etc-default-rabbitmq-server.erb'),
-    require => Package['rabbitmq-server'],
+  if $::systemd {
+    include ::systemd
+    file { '/etc/systemd/system/rabbitmq-server.service.d':
+      ensure => directory,
+      owner  => 'root',
+      group  => 'root',
+    }
+
+    file { '/etc/systemd/system/rabbitmq-server.service.d/limits.conf':
+      ensure  => present,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => template('rabbitmq/rabbitmq-server.service.d-limits.conf.erb'),
+      require => File['/etc/systemd/system/rabbitmq-server.service.d'],
+      notify  => Exec['systemctl-daemon-reload']
+    }
+  }
+  else {
+    file { '/etc/default/rabbitmq-server':
+      ensure  => present,
+      content => template('rabbitmq/etc-default-rabbitmq-server.erb'),
+      require => Package['rabbitmq-server'],
+    }
   }
 
   file {['/etc/rabbitmq/ssl', '/etc/rabbitmq/rabbitmq.conf.d']:
