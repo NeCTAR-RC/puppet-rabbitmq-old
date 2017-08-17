a615956a3c399905babcf21497a90d4fe182db45
Andy Botting <andy@andybotting.com>
2017-08-17 14:04:45 +1000

    Increase timeout for nagios queue check
    Change-Id: Ic42a2073a7c6059c462fd5423b8931765f54a773

--- manifests/init.pp
+++ manifests/init.pp
@@ -102,7 +102,7 @@ class rabbitmq(
     'rabbitmq_overview':
       servicegroups => 'message-queues',
       check_command => "/usr/local/lib/nagios/plugins/check_rabbitmq_overview -H ${::fqdn} --port ${mgmt_port} -c ${nagios_critical} -w ${nagios_warning} -u ${user} -p ${password} ${ssl}",
-      nrpe_command  => 'check_nrpe_slow_1arg';
+      nrpe_command  => 'check_nrpe_really_slow_1arg';
     'rabbitmq_aliveness':
       servicegroups => 'message-queues',
       check_command => "/usr/local/lib/nagios/plugins/check_rabbitmq_aliveness -H ${::fqdn} --port ${mgmt_port} --vhost ${vhost} -u ${user} -p ${password} ${ssl}";
