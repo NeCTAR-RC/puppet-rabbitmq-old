6d36dcf08f30048172a9a777e6f6541b573b1caa
Andy Botting <andy@andybotting.com>
2018-12-21 10:13:59 +1100

    Switch to libmonitoring-plugin-perl for nagios check
    libnagios-plugin-perl has been deprecated and libmonitoring-plugin-perl
    has been added in its place.
    
    Xenial supported both, but only libmonitoring-plugin-perl is
    available in Bionic. As we have no rabbitmq servers running that
    are older than Xenial, we'll just move to libmonitoring-plugin-perl
    for the nagios test.
    
    Change-Id: I52d0e1df03f67a09b5480164d1571b38d950efa5

--- manifests/init.pp
+++ manifests/init.pp
@@ -115,10 +115,7 @@ class rabbitmq(
       check_command => "/usr/local/lib/nagios/plugins/check_rabbitmq_aliveness -H ${::fqdn} --port ${mgmt_port} --vhost ${vhost} -u ${user} -p ${password} ${ssl}";
   }
 
-  ensure_packages(['libwww-perl', 'libjson-perl', 'python-requests'])
-  if $::lsbdistcodename in ['precise','trusty','xenial'] {
-    ensure_packages(['libnagios-plugin-perl'])
-  }
+  ensure_packages(['libmonitoring-plugin-perl', 'libwww-perl', 'libjson-perl', 'python-requests'])
 
   file {
     '/usr/local/lib/nagios/plugins/check_rabbitmq_overview':
